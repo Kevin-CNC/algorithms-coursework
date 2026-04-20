e9670eed714b04e47b669c65264dcbcc49539ca3
Kevin (Work-Laptop) <Kevin.Caplescu@commscope.com>
2026-04-21 00:54:59 +0100

    created point class for orientation test

---
+++ orientationTest/point.java
@@ -0,0 +1,14 @@
+package orientationTest;
+
+// point class for ease of implementation
+public class point {
+    int x;
+    int y;
+
+
+    public point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+}
