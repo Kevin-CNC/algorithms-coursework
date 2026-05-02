978a960f591bfe8b808ebfafd11274a6c00b6922
Kevin Caplescu <kevincncaplescu@gmail.com>
2026-05-02 13:34:16 +0100

    improved implementation by using doubles

---
+++ ShoelaceFormula/point.java
@@ -0,0 +1,14 @@
+package ShoelaceFormula;
+
+// point class for ease of implementation
+public class point {
+    double x;
+    double y;
+
+
+    public point(double  x, double  y){
+        this.x = x;
+        this.y = y;
+    }
+
+}
