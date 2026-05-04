b744067da3eb4fa6358fd903bd655fcfc29847fd
Kevin Caplescu <kevincncaplescu@gmail.com>
2026-05-04 14:12:33 +0100

    fixed convex hull implementation

--- convexHull/convexHullImplementation.java
+++ convexHull/convexHullImplementation.java
@@ -34,21 +34,55 @@ public class convexHullImplementation {
         int currentP = leftMost;
         int nextP;
 
-        while (currentP != leftMost){
+        do {
             hullkStack.add(points[currentP]);
-            // using the `orientation` method we find the most counter-clockwise point from current
-            nextP = 0; // default start with 0
+            nextP = 0;
             for (int i = 1; i < pointsL; i++) {
-                // If orientation returns 1, points[i] is counter-clockwise of the line from currentP, so we add it to the stack
-                // always check that the current point is not the same as the chosen 'next'
                 if (nextP == currentP ||
                     orientation(points[currentP], points[nextP], points[i]) == 1) {
                     nextP = i;
                 }
             }
-            currentP = nextP; // advance to the next hull vertex
-        }
+            currentP = nextP;
+        } while (currentP != leftMost);
         return hullkStack;
+    }
+
+
+    public static void main(String[] args) {
+
+        // Case 1: Triangle set
+        point c1P1 = new point(0,0);
+        point c1P2 = new point(7,0);
+        point c1P3 = new point(7,7);
+        
+        point[] points = {c1P1, c1P2, c1P3};
+
+        List<point> case1Hull = convexHull(points);
+        System.out.println("Convex Hull:");
+        for (point p : case1Hull) {
+            System.out.println("(" + p.x + ", " + p.y + ")");
+        }
+
+
+        // Case 2: 9-Points set
+        point c2P1 = new point(1, 1);
+        point c2P2 = new point(8, 2);
+        point c2P3 = new point(7, 3);
+        point c2P4 = new point(8, 4);
+        point c2P5 = new point(5, 5);
+        point c2P6 = new point(6, 4);
+        point c2P7 = new point(2, 4);
+        point c2P8 = new point(3, 3);
+        point c2P9 = new point(6, 1);
+
+        point[] points2 = {c2P1, c2P2, c2P3, c2P4, c2P5, c2P6, c2P7, c2P8, c2P9};
+
+        List<point> case2Hull = convexHull(points2);
+        System.out.println("Convex Hull:");
+        for (point p : case2Hull) {
+            System.out.println("(" + p.x + ", " + p.y + ")");
+        }
     }
 
 }
